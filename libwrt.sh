7709389e1c63b7279c6a691605cfa018bbd0da72
katr <katriyda@outlook.com>
2026-08-12 16:50:11 +0800

    修改默认管理地址为 10.10.10.1（LAN 10.10.10.0/24）
    USB 联网依赖（usb-modeswitch/usbmuxd/usbutils/kmod-usb-net-rndis 等）原版 config 已全部包含，无需修改。

--- libwrt.sh
+++ libwrt.sh
@@ -1,3 +1,5 @@
 rm -rf package/emortal/luci-app-athena-led
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
+# 修改默认管理地址为 10.10.10.1（LAN 10.10.10.0/24）
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
