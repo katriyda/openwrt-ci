67d0fed50cb1ab0988adfb3d159cbba5e85e4f8f
katr <katriyda@outlook.com>
2026-08-12 19:34:09 +0800

    默认中文 + argon 主题 + 主机名 ZN-M2 + natmap
    - config：加 luci-i18n-base-zh-cn（默认中文）、luci-theme-argon + argon-config + 中文包（默认主题）、natmap + luci-app-natmap（内网穿透，ImmortalWrt 源内置）
    - config：修正 wolplus（无效包名）为 luci-app-wol
    - libwrt.sh：hostname LibWrt→ZN-M2、lang auto→zh_cn、mediaurlbase bootstrap→argon

--- libwrt.sh
+++ libwrt.sh
@@ -3,3 +3,9 @@ git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 # 修改默认管理地址为 10.10.10.1（LAN 10.10.10.0/24）
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+# 设备名称 LibWrt 改为 ZN-M2
+sed -i "s/hostname='LibWrt'/hostname='ZN-M2'/g" package/base-files/files/bin/config_generate
+# 默认中文语言
+sed -i "s/option lang auto/option lang zh_cn/g" feeds/luci/modules/luci-base/root/etc/config/luci
+# 默认 argon 主题
+sed -i "s#option mediaurlbase /luci-static/bootstrap#option mediaurlbase /luci-static/argon#g" feeds/luci/modules/luci-base/root/etc/config/luci
